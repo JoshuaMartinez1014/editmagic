78ae51cb08de9765aa22083ff8e52dd337b790cc
JoshuaMartinez1014 <Joshaumartinez1014@gmail.com>
2024-02-28 15:52:21 -0600

    Connect and Make DB Models

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,3 +1,9 @@
+/**
+ * Utility function to merge class names using clsx and tailwind-merge.
+ * @param inputs Class names or objects to merge.
+ * @returns Merged class names string.
+ */
+
 import { type ClassValue, clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
 
