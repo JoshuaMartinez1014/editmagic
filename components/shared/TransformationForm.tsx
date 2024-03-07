ae9876ece8a79742239a7b9201f0b75da06dee07
JoshuaMartinez1014 <Joshaumartinez1014@gmail.com>
2024-03-07 08:39:21 -0600

    Implement wireframe for image upload and completed image

--- components/shared/TransformationForm.tsx
+++ components/shared/TransformationForm.tsx
@@ -34,6 +34,7 @@ import { AspectRatioKey, debounce, deepMergeObjects } from "@/lib/utils";
 import { fields } from "@hookform/resolvers/ajv/src/__tests__/__fixtures__/data.js";
 import { updateCredits } from "@/lib/actions/user.actions";
 import MediaUploader from "./MediaUploader";
+import TransformedImage from "./TransformedImage";
 
 export const formSchema = z.object({
   title: z.string(),
@@ -118,7 +119,7 @@ const transformationForm = ({
     debounce(() => {}, 1000);
   };
 
-  // TODO: Return to updateCredits
+  // --- UPDATE CREDIT FEE IF NEEDED ---
   const onTransformHandler = async () => {
     setIsTransforming(true);
 
@@ -129,7 +130,7 @@ const transformationForm = ({
     setNewTransformation(null);
 
     startTransition(async () => {
-      // await updateCredits(userId, creditFee)
+      await updateCredits(userId, -1);
     });
   };
 
@@ -221,23 +222,31 @@ const transformationForm = ({
           </div>
         )}
 
-                    <div className="media-uploader-field">
-                      <CustomField 
-                      control={form.control}
-                      name="publicId"
-                      className="flex size-full flex-col"
-                      render={({field}) => (
-                        <MediaUploader 
-                        onValueChange={field.onChange}
-                        setImage={setImage}
-                        publicId={field.value}
-                        image={image}
-                        type={type}
-                        />
-                      )}
-                      />
-                    </div>
+        <div className="media-uploader-field">
+          <CustomField
+            control={form.control}
+            name="publicId"
+            className="flex size-full flex-col"
+            render={({ field }) => (
+              <MediaUploader
+                onValueChange={field.onChange}
+                setImage={setImage}
+                publicId={field.value}
+                image={image}
+                type={type}
+              />
+            )}
+          />
 
+          <TransformedImage
+            image={image}
+            type={type}
+            title={form.getValues().title}
+            isTransforming={isTransforming}
+            setIsTransforming={setIsTransforming}
+            transformationConfig={transformationConfig}
+          />
+        </div>
 
         <div className="flex flex-col gap-4">
           <Button
